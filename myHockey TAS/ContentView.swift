3a321763cf211465d449ddac8f3fb6c704ba08f6
bmoxey <bmoxey@gmail.com>
2024-01-06 01:33:07 +1100

    Initial Commit

---
+++ myHockey TAS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myHockey TAS
+//
+//  Created by Brett Moxey on 6/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myHockey TAS/myHockey_TASApp.swift
@@ -0,0 +1,17 @@
+//
+//  myHockey_TASApp.swift
+//  myHockey TAS
+//
+//  Created by Brett Moxey on 6/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct myHockey_TASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
